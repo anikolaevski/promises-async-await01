54e5a23c7e355d82615a51c147327429d96dc8c2
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2019-12-20 01:37:52 +0300

    coverage to do

--- test/GameSavingLoader.test.js
+++ test/GameSavingLoader.test.js
@@ -2,20 +2,21 @@ import GameSavingLoader from '../src/js/GameSavingLoader';
 import json from '../src/js/parser';
 import read from '../src/js/reader';
 
+const UniTestData = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+
 test('Test GameSavingLoader', () => {
-  const expected = JSON.parse('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
+  const expected = JSON.parse(UniTestData);
   GameSavingLoader.load().then((data) => {
     const result = JSON.parse(data);
     expect(result).toEqual(expected);
   });
 });
 
-
 test('Test read & parse', () => {
-  const expected = JSON.parse('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
+  const expected = UniTestData;
   read().then((dat) => {
-    json(dat).then((data) => {
-      const result = JSON.parse(data);
+    json(dat).then((result) => {
+      // const result = JSON.parse(data);
       expect(result).toEqual(expected);
     });
   });
